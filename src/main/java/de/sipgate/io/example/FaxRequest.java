59b323f6ba7e333dccfa19064dfdb05797545b98
sipgate.io Team <io-team@sipgate.de>
2019-07-18 09:34:46 +0000

    docs: improved wording in README.md

---
+++ src/main/java/de/sipgate/io/example/FaxRequest.java
@@ -0,0 +1,17 @@
+package de.sipgate.io.example;
+
+public class FaxRequest {
+
+	public String faxlineId;
+	public String recipient;
+	public String filename;
+	public String base64Content;
+
+	public FaxRequest(String faxlineId, String recipient, String filename, String base64Content) {
+		this.faxlineId = faxlineId;
+		this.recipient = recipient;
+		this.filename = filename;
+		this.base64Content = base64Content;
+	}
+
+}
